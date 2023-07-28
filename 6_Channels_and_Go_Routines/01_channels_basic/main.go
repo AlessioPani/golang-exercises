55fd16ab516df8a43bdb593294e63f47719037b9
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2023-07-28 19:36:48 +0200

    Added WaitGroup in the basic example

--- 6_Channels_and_Go_Routines/01_channels_basic/main.go
+++ 6_Channels_and_Go_Routines/01_channels_basic/main.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(s)
 }
 
 func main() {
 
-	go printSomething("This is the first thing to be printed!")
+	// WaitGroup to wait the end of each goroutines before exiting the program
+	var wg sync.WaitGroup
 
-	time.Sleep(1 * time.Second)
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"eta",
+		"theta",
+		"zeta",
+		"epsilon",
+		"pi",
+	}
 
-	printSomething("This is the second thing to be printed!")
+	wg.Add(len(words))
 
+	for i, w := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, w), &wg)
+	}
+
+	wg.Wait()
+
+	// Add 1 to WaitGroup not to have a negative value
+	// before calling wg.Done()
+	wg.Add(1)
+	printSomething("This is the second thing to be printed", &wg)
 }
