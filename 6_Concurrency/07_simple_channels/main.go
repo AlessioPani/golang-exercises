3cb8d1bb7fa97eeaca382536dd4ee454195127d1
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2023-08-08 14:19:00 +0200

    Added comments to simple channels example

--- 6_Concurrency/07_simple_channels/main.go
+++ 6_Concurrency/07_simple_channels/main.go
@@ -5,6 +5,10 @@ import (
 	"strings"
 )
 
+// shout has two parameters: a receive only chan ping, and a send only chan pong.
+// Note the use of <- in function signature. It simply takes whatever
+// string it gets from the ping channel,  converts it to uppercase and
+// appends a few exclamation marks, and then sends the transformed text to the pong channel.
 func shout(ping <-chan string, pong chan<- string) {
 	for {
 		s := <-ping
@@ -17,6 +21,7 @@ func main() {
 	ping := make(chan string) // RX
 	pong := make(chan string) // TX
 
+	// start a goroutine
 	go shout(ping, pong)
 
 	fmt.Println("Type something and press ENTER (enter Q to quit)")
@@ -33,14 +38,21 @@ func main() {
 			break
 		}
 
+		// send userInput to "ping" channel
 		ping <- userInput
+
+		// wait for a response from the pong channel. Again, program
+		// blocks (pauses) until it receives something from
+		// that channel.
 		response := <-pong
 
+		// print the response to the console.
 		fmt.Println("Response:", response)
-
 	}
 
 	fmt.Println("All done, closing channels...")
+
+	// close the channels
 	close(ping)
 	close(pong)
 }
